25ac26c110e5a64fd87c2d1fc69cd962e3236b28
sborshikHlama <logrus.928@gmail.com>
2023-02-01 23:45:32 +0100

    move clear function to CanvasContainer

--- src/components/canvas/useDraw.ts
+++ src/components/canvas/useDraw.ts
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from "react"
 
+//Custom hook for drawing line
 export const useDraw = (onDraw: ({currentPoint, prevPoint}: Draw) => void) => {
+    
     const [mouseDown, setMouseDown] = useState(false)
 
     const canvasRef = useRef<HTMLCanvasElement>(null)
@@ -8,17 +10,19 @@ export const useDraw = (onDraw: ({currentPoint, prevPoint}: Draw) => void) => {
     
     const onMouseDown = () => setMouseDown(true)
 
-    const clear = () => {
-            const canvas = canvasRef.current
-            if(!canvas) return
-
-            const ctx = canvas.getContext('2d')
-            if(!ctx) return
-
-            ctx.clearRect(0, 0, canvas.width, canvas.height)
-    }
-
     useEffect(() => {
+
+        const computePointInCanvas = (e: MouseEvent) => {
+            const canvas = canvasRef.current
+            if (!canvas) return 
+
+            const rect = canvas.getBoundingClientRect()
+            const x = e.clientX - rect.left
+            const y = e.clientY - rect.top
+
+            return { x, y }
+        }
+        
         const handler = (e: MouseEvent) => {
             if(!mouseDown) return
             const currentPoint = computePointInCanvas(e)
@@ -31,16 +35,6 @@ export const useDraw = (onDraw: ({currentPoint, prevPoint}: Draw) => void) => {
 
         }
 
-        const computePointInCanvas = (e: MouseEvent) => {
-            const canvas = canvasRef.current
-            if (!canvas) return 
-
-            const rect = canvas.getBoundingClientRect()
-            const x = e.clientX - rect.left
-            const y = e.clientY - rect.top
-
-            return { x, y }
-        }
 
         const mouseUpHandler = () => {
             setMouseDown(false)
@@ -56,5 +50,5 @@ export const useDraw = (onDraw: ({currentPoint, prevPoint}: Draw) => void) => {
         }
     }, [onDraw])
 
-    return{canvasRef, onMouseDown, clear}
+    return{canvasRef, onMouseDown}
 }
