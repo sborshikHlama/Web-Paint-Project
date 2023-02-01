bf57fc397197ce073d74699d2e39ff793a8a6674
sborshikHlama <logrus.928@gmail.com>
2023-02-01 23:41:10 +0100

    remove controller from CanvasContainer

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { CanvasContainer } from '../components/canvas/CanvasContainer';
 import { Header } from '../components/Header';
@@ -6,37 +6,38 @@ import { checkIsTool } from '../types/TypeCheckFunctions';
 import { checkIsWeight } from '../types/TypeCheckFunctions';
 import { checkIsColor } from '../types/TypeCheckFunctions';
 
+
 function App() {
 
+//States for controlling App
 const [tool, setTool] = useState<Tool>( "✏️")
 const [weight, setWeight] = useState<LineWeight>("normal")
 const [color, setColor] = useState<Color>("black")
 
+// This object is only for tracking states and conosle.log them
 const controller: Controller = {
   tool,
   weight,
   color
 }
 
+useEffect(() => console.log(controller), [controller])
+
+//Function that controlls states
 const handleStates: HandleStates = (updateItem) => {
 
-  if(checkIsTool(updateItem)) {
-    setTool(updateItem)
-  }
-  if(checkIsWeight(updateItem)) {
-    setWeight(updateItem)
-  }
-  if(checkIsColor(updateItem)) {
-    setColor(updateItem)
-  } 
-  console.log(controller)
+  if(checkIsTool(updateItem)) setTool(updateItem)
+
+  if(checkIsWeight(updateItem)) setWeight(updateItem)  
+
+  if(checkIsColor(updateItem)) setColor(updateItem)
 }
 
   return (
     <>
       <h1>Web Paint Project</h1>
-      <Header handleStates={handleStates} controller={controller}/>
-      <CanvasContainer controller={controller}/>
+      <Header handleStates={handleStates} color={color}/>
+      <CanvasContainer weight={weight} color={color} tool={tool} handleStates={handleStates}/>
     </>
   );
 }
